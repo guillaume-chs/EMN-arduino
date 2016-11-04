0ad52d65adaa7b6d66e90c16731fb8e273f783fc
Guillaume Chanson <guillaume.chns@gmail.com>
2016-11-04 15:37:12 +0100

    Implement Grid drawing

--- lcd-gds.cpp
+++ lcd-gds.cpp
@@ -21,12 +21,12 @@
 // JOYSTICK //
 //////////////
 
-#define NONE  -1
-#define LEFT  0
+#define NONE    -1
+#define LEFT    0
 #define CLICK   1
 #define BOTTOM  2
 #define RIGHT   3
-#define TOP   4
+#define TOP     4
 
 uint8_t uiKeyCodeFirst = NONE;
 uint8_t uiKeyCodeSecond = NONE;
@@ -36,16 +36,28 @@ int key = NONE;
 int oldkey = NONE;
 
 
+//////////////
+// JOYSTICK //
+//////////////
+
+#define STEP_MENU 1
+#define STEP_GAME 2
+
+uint8_t STEP = STEP_MENU;
+
+
+
 ////////////
 //  MENU  //
 ////////////
 
 #define MENU_ITEMS 4
-char *menu_strings[MENU_ITEMS] = { "Ping", "Pong", "Pouet", "Click" };
+char *menu_strings[MENU_ITEMS] = { "Damien", "Steeve", "Guillaume", "C'est parti !" };
 
 uint8_t menu_current = 0;
 uint8_t last_key_code = NONE;
 bool menu_redraw_required = true;
+bool game_redraw_required = false;
 
 
 
@@ -56,6 +68,7 @@ bool menu_redraw_required = true;
 
 void uiStep(void);
 void drawMenu(void);
+void drawGame(void);
 void updateMenu(void);
 int get_joystick_key(unsigned int);
 char *get_joystick_name(int key);
@@ -63,22 +76,38 @@ char *get_joystick_name(int key);
 U8GLIB_NHD_C12864 u8g(13, 11, 10, 9, 8);
 
 void setup() {
-  Serial.begin(9600);
   u8g.setRot180();      // rotate screen, if required
   u8g.setContrast(0);
+
+  u8g.setFont(u8g_font_6x12);//4x6 5x7 5x8 6x10 6x12 6x13
+  u8g.setFontRefHeightText();
+  u8g.setFontPosTop();
+  
   menu_redraw_required = true; // force initial redraw
+  game_redraw_required = false;
 }
 
 TASK(periodicTask) {  
   uiStep();              // check for key press
-  updateMenu();          // update menu bar    
   
-  if (menu_redraw_required) {
-    u8g.firstPage();
-    do  {
-      drawMenu();
-    } while(u8g.nextPage());
-    menu_redraw_required = false;
+  if (STEP == STEP_MENU) {
+    updateMenu();          // update menu bar
+    
+    if (menu_redraw_required) {
+      u8g.firstPage();
+      do {
+        drawMenu();
+      } while(u8g.nextPage());
+      menu_redraw_required = false;
+    }
+  } else {
+    if (game_redraw_required) {
+      u8g.firstPage();
+      do {
+        drawGame();
+      } while(u8g.nextPage());
+      game_redraw_required = false;
+    }
   }
 }
 
@@ -92,7 +121,6 @@ void uiStep(void) {
   if (key != oldkey) {			
     oldkey = key;
     if (key > NONE) {
-      Serial.println(get_joystick_name(key));
       uiKeyCodeFirst = key;
       uiKeyCode = uiKeyCodeFirst;           
     }
@@ -106,11 +134,6 @@ void uiStep(void) {
 void drawMenu(void) {
   uint8_t i, h;
   u8g_uint_t w, d;
-
-  u8g.setContrast(0);
-  u8g.setFont(u8g_font_6x12);//4x6 5x7 5x8 6x10 6x12 6x13
-  u8g.setFontRefHeightText();
-  u8g.setFontPosTop();
   
   h = u8g.getFontAscent() - u8g.getFontDescent();
   w = u8g.getWidth();
@@ -125,8 +148,45 @@ void drawMenu(void) {
   }
 }
 
-void updateMenu(void) 
-{
+void drawGame(void) {
+  uint8_t i, font_height;
+  u8g_uint_t width, height, width_ratio;
+
+  font_height = u8g.getFontAscent() - u8g.getFontDescent();
+  width = u8g.getWidth();
+  height = u8g.getHeight();
+  width_ratio = (width/5);
+
+  u8g.setDefaultForegroundColor();
+  
+  // First line
+  i = 0;
+  u8g.drawBox(0, i, width, font_height);
+  u8g.setDefaultBackgroundColor();
+  u8g.drawStr(3, i, "J1 : 0");
+  u8g.drawStr(width - 3 - u8g.getStrWidth("3.4 : Damien"), i, "3.4 : Damien");
+  i = font_height*1.5 + 1;
+  u8g.setDefaultForegroundColor();
+
+  // Column lines
+  int y = 3*(font_height+1);
+  for (int j = 0; j <= 3; j++) {
+    int x = (1+j)*(width_ratio+1);
+    u8g.drawLine(x, i, x, i+y);
+  }
+
+  // Grid
+  for (int line = 1; line <= 3; line++) {
+    u8g.drawLine(width_ratio, i, width-width_ratio, i);
+    i += font_height + 1;
+  }
+  u8g.drawLine(width_ratio, i, width-width_ratio, i);
+  i += font_height/2 + 1;
+
+  u8g.drawBox(0, i, width, font_height);
+}
+
+void updateMenu(void) {
   switch (uiKeyCode) {
     case BOTTOM:
       menu_current++;
@@ -142,8 +202,15 @@ void updateMenu(void)
       menu_current--;
       menu_redraw_required = true;
       break;
+
+    case CLICK:
+      if (menu_current == MENU_ITEMS - 1) {
+        STEP = STEP_GAME;
+        menu_redraw_required = false;
+        game_redraw_required = true;
+      }
   }
-  uiKeyCode = CLICK;
+  uiKeyCode = NONE;
 }
 
 
@@ -157,8 +224,7 @@ void updateMenu(void)
 //   0 --  1 -- 3
 //         |
 //         2
-int get_joystick_key(unsigned int input)
-{   
+int get_joystick_key(unsigned int input) {   
     if (input < 100) return LEFT;
     else  if (input < 300) return CLICK;
     else  if (input < 500) return BOTTOM;
@@ -169,32 +235,14 @@ int get_joystick_key(unsigned int input)
 
 char *get_joystick_name(int key) {
   char *name;
-
+  
   switch(key) {
-    case LEFT:
-      name = "LEFT";
-      break;
-
-    case RIGHT:
-      name = "RIGHT";
-      break;
-
-    case TOP:
-      name = "TOP";
-      break;
-
-    case BOTTOM:
-      name = "BOTTOM";
-      break;
-
-    case CLICK:
-      name = "CLICK";
-      break;
-
-    default:
-      name = "NONE";
-      break;
+    case LEFT:   name = "LEFT";   break;
+    case RIGHT:  name = "RIGHT";  break;
+    case TOP:    name = "TOP";    break;
+    case BOTTOM: name = "BOTTOM"; break;
+    case CLICK:  name = "CLICK";  break;
+    default:     name = "NONE";   break;
   }
-
   return name;
 }
